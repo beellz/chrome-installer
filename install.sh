4011297309d925a2856fcf3637990fab07f4d7cb
beellz <sayedbelal@Sayeds-MacBook-Pro.local>
2017-02-25 23:39:56 +0530

    update and upgrade

---
+++ install.sh
@@ -0,0 +1,13 @@
+
+sudo apt-get update
+echo "done "
+echo "done "
+echo "done"
+echo "done"
+sudo apt-get -y upgrade
+echo "update and upgrade done "
+echo "done "
+sudo apt-get install nginx
+echo "install doen nginx"
+sudo apt-get install mysql-server
+sudo mysql_install_db
