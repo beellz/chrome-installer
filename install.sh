3822cfde40950c4b1e27ee7aca2795795d25e9fe
beellz <sayedbelal@Sayeds-MacBook-Pro.local>
2017-02-25 23:46:24 +0530

    removed echo and added automatic yes

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 
 sudo apt-get update
 echo "done "
-echo "done "
-echo "done"
-echo "done"
 sudo apt-get -y upgrade
 echo "update and upgrade done "
 echo "done "
-sudo apt-get install nginx -y 
+sudo apt-get -Y install nginx -y 
 echo "install done nginx"
-sudo apt-get install mysql-server
+sudo apt-get -y install mysql-server
 sudo mysql_install_db
