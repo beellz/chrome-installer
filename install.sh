f1d49c094a8382e000c8fcf6fe51d6b404d591fa
beellz <sayedbelal@Sayeds-MacBook-Pro.local>
2017-02-26 00:56:38 +0530

    changed -y

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "done "
 sudo apt-get -y upgrade
 echo "update and upgrade done "
 echo "done "
-sudo apt-get -Y install nginx -y 
+sudo apt-get -y install nginx -y 
 echo "install done nginx"
 sudo apt-get -y install mysql-server
 sudo mysql_install_db
