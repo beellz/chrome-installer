6e8efab2fae1a743342153bfb0f299466c040966
beellz <sayedbelal@Sayeds-MacBook-Pro.local>
2017-02-25 23:44:16 +0530

    typo removed

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "done"
 sudo apt-get -y upgrade
 echo "update and upgrade done "
 echo "done "
-sudo apt-get install nginx
-echo "install doen nginx"
+sudo apt-get install nginx -y 
+echo "install done nginx"
 sudo apt-get install mysql-server
 sudo mysql_install_db
